274bca3e9a86b143fa03f130c0eddfc8ae2e0d18
Nana Adjei Manu <n.k.a.manu06@gmail.com>
2024-09-13 01:29:55 +0100

    chore: init tests

---
+++ src/test/java/dev/nmanu/CracheTest.java
@@ -0,0 +1,26 @@
+package dev.nmanu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CracheTest {
+
+    @org.junit.jupiter.api.Test
+    void getStore() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void get() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void put() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void evict() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEvictionList() {
+    }
+}
